c8fd9166fd68c96272bbd989d7e5df53c31e1d50
Monika <monika.zemankiewicz.2015@gmail.com>
2023-10-22 22:42:16 +0200

    some basic routing

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,1 +1,30 @@
-console.log("js is loaded");
+const router = async () => {
+  const routes = [
+    { path: "/", view: () => console.log("Viewing home") },
+    { path: "/animations", view: () => console.log("Viewing animations") },
+    { path: "/other", view: () => console.log("Viewing other") },
+  ];
+
+  //Test each route for potential match
+  const potentialMatches = routes.map((route) => {
+    return {
+      route: route,
+      isMatch: location.pathname === route.path,
+    };
+  });
+
+  let match = potentialMatches.find((potentialMatch) => potentialMatch.isMatch);
+
+  if (!match) {
+    match = {
+      route: routes[0],
+      isMatch: true,
+    };
+  }
+
+  console.log(match);
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  router();
+});
